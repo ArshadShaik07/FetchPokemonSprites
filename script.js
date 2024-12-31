c65c2bb988d180da699fd5fd57121ff5fe9b6b0e
Arshad-jpeg <arshadshaik2007@gmail.com>
2024-12-31 20:01:49 +0530

    .

---
+++ script.js
@@ -0,0 +1,28 @@
+let pokemonName;
+
+function searchPokemon() {
+  pokemonName = document.querySelector(".pokemon-search").value.toLowerCase();
+  document.querySelector(".pokemon-search").value = "";
+  displayPokemon();
+}
+
+let pokemonImg = document.querySelector(".pokemon-img");
+function displayPokemon() {
+  const pokemon = fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+  pokemon
+    .then((response) => {
+      if (!response.ok) {
+        alert("Pokemon Not Found!");
+        throw new Error("Pokemon Not Found!");
+      } else {
+        return response.json();
+      }
+    })
+    .then((pokemonData) => {
+      pokemonImg.src = pokemonData.sprites.front_default;
+      pokemonImg.style.display = "inline";
+    })
+    .catch((error) => {
+      console.error("Pokemon not found");
+    });
+}
